040b79d9a294c89712d40595a7d2292a13326a8e
dannyfeng (Yanming) <u6611178@anu.edu.au>
2018-02-25 13:56:02 +1100

    finished Name and passed all tests

---
+++ src/comp1110/homework/J04/Name.java
@@ -0,0 +1,14 @@
+package comp1110.homework.J04;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+        String a = in.nextLine();
+        System.out.println(a.toUpperCase());
+        System.out.println(a.length());
+    }
+}
