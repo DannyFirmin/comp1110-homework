746fd8ae70476dc4d27bb8d454bb7b932702085a
dannyfeng (Yanming) <u6611178@anu.edu.au>
2018-03-03 14:18:43 +1100

    finished Prime and passed all tests

---
+++ src/comp1110/homework/J06/Prime.java
@@ -0,0 +1,25 @@
+package comp1110.homework.J06;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+        boolean primeYes = true;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primeYes = false;
+                    break;
+                }
+            }
+            if (primeYes)
+                System.out.println(i);
+            primeYes = true;
+
+        }
+    }
+}
