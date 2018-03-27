ee8f677298f7f90cfa37ca8cc88d0179a951dfe8
Danny Feng <u6611178@anu.edu.au>
2018-03-27 13:28:20 +1100

    just start

---
+++ src/comp1110/homework/O04/Sum.java
@@ -0,0 +1,19 @@
+package comp1110.homework.O04;
+
+import java.util.ArrayList;
+
+public class Sum extends ArrayList {
+    ArrayList <Integer> arrayList = new ArrayList <Integer> ();
+
+//    int sum() {
+//        for (int integer : element){
+//            arrayList.add(integer);
+//        }
+//
+//        for (int i = 1; i< arrayList.size();i++){
+//            arrayList.set (i, ( i + 1 ) * 100 + arrayList.get (i - 1));
+//
+//    }
+//    return (arrayList);
+//}
+}
