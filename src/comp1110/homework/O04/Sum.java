8a3494b72d7d00efff4b496c4deedec9bf8984cd
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-04-18 09:00:04 +1000

    finished Sum and passed all tests

--- src/comp1110/homework/O04/Sum.java
+++ src/comp1110/homework/O04/Sum.java
@@ -1,19 +1,38 @@
 package comp1110.homework.O04;
 
+
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class Sum extends ArrayList {
-    ArrayList <Integer> arrayList = new ArrayList <Integer> ();
+public class Sum extends ArrayList<Integer> {
+    public int sum = 0;
 
-//    int sum() {
-//        for (int integer : element){
-//            arrayList.add(integer);
-//        }
-//
-//        for (int i = 1; i< arrayList.size();i++){
-//            arrayList.set (i, ( i + 1 ) * 100 + arrayList.get (i - 1));
-//
-//    }
-//    return (arrayList);
-//}
+    public int sum() {
+        return this.sum;
+    }
+
+    public boolean add(Integer i) {
+        this.sum = sum + i;
+        return super.add(i);
+    }
+
+    public void add(int index, Integer i) {
+        super.add(index, i);
+        this.sum = sum + i;
+    }
+
+    public boolean addAll(Collection<? extends Integer> c) {
+        for (int i : c) {
+            this.sum = sum + i;
+        }
+        return super.addAll(c);
+    }
+
+    public boolean addAll(int index, Collection<? extends Integer> c) {
+        for (int i : c) {
+            this.sum = sum + i;
+        }
+        return super.addAll(index, c);
+
+    }
 }
