f4e4b9b9e24980eaaa05326c4cb4796f10e3a59f
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-06-09 16:00:57 +1000

    added Josh's solution

--- src/comp1110/homework/J14/Cost.java
+++ src/comp1110/homework/J14/Cost.java
@@ -1,77 +1,47 @@
 package comp1110.homework.J14;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Cost {
-    HashMap<String, Item> hs;
-    ArrayList<Item> al;
-    int SmallestWaste;
+    HashMap<String, Item> items = new HashMap<>();
 
-    public Cost() {
-        this.hs = new HashMap<String, Item>();
-        this.al = new ArrayList<Item>();
+    void addItem(String name, int cost) {
+        items.put(name, new Item(name, cost));
     }
 
-    public void addItem(String name, int cost) {
-        Item it = new Item(name, cost);
-        hs.put(name, it);
-        al.add(it);
-    }
-
-    public int voucherWaste(int value) {
-        SmallestWaste = value;
-        int[] priceList = new int[al.size()];
-        for (int i = 0; i < al.size(); i++) {
-            priceList[i] = al.get(i).cost;
+    void printCost() {
+        List<Item> sorted = new ArrayList<>();
+        for (Item item : items.values()) {
+            sorted.add(item);
         }
-        buyToEnd(priceList, 0, value);
-        System.out.println(SmallestWaste);
-        return SmallestWaste;
-
-        /*
-         * Every product have two filed:
-         *   1. how many can the voucher buy?
-         *   2. how much money left?
-         * */
+        Collections.sort(sorted);
+        for (Item item : sorted) {
+            System.out.println(item.name + " " + item.cost);
+        }
     }
 
-    public void buyToEnd(int prices[], int i, int leftMoney) {
-        if (this.SmallestWaste == 0) {
-            return;
+    int voucherWaste(int value) {
+        Set<Integer> prices = new HashSet<>();
+        for (Item item : items.values()) {
+            prices.add(item.cost);
         }
-        System.out.println("i:" + i + " leftMoney:" + leftMoney);
-        if (i == prices.length - 1) {
-            if (this.SmallestWaste > leftMoney % prices[i]) {
-                this.SmallestWaste = leftMoney % prices[i];
+        return remainingVoucherWaste(value, prices);
+    }
+
+    private int remainingVoucherWaste(int value, Set<Integer> prices) {
+        int min = value;
+        if(min == 0) return 0;
+        for (Integer p : prices) {
+            if (p <= value) {
+                int newValue = remainingVoucherWaste(value - p, prices);
+                if (newValue < min) {
+
+                    min = newValue;
+                }
             }
-            return;
         }
-        for (int j = leftMoney / prices[i]; j >= 0; j--) {
-            int nextLeft = leftMoney - prices[i] * j;
-            buyToEnd(prices, i + 1, nextLeft);
-        }
+        return min;
     }
-
-    public static void main(String[] args) {
-        Cost cost = new Cost();
-        cost.addItem("A", 243);
-        cost.addItem("B", 401);
-        cost.addItem("C", 245);
-        cost.addItem("D", 1004);
-        cost.addItem("E", 297);
-        cost.voucherWaste(2007);
-    }
-
-    public void printCost() {
-        al.sort(Item::compareTo);
-        for (Item item : al) {
-            System.out.println(item.toString());
-        }
-    }
-
-
-
 
     public class Item implements Comparable<Item> {
         String name;
@@ -86,11 +56,106 @@ public class Cost {
             return name + " " + cost;
         }
 
-        public int compareTo(Item item) {
-            return this.cost - item.cost;
+        @Override
+        public int compareTo(Item o) {
+            int diff = this.cost - o.cost;
+            if (diff == 0) return this.name.compareTo(o.name);
+            else return diff;
         }
     }
 }
+//Code from Josh
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//public class Cost {
+//    HashMap<String, Item> hs;
+//    ArrayList<Item> al;
+//    int SmallestWaste;
+//
+//    public Cost() {
+//        this.hs = new HashMap<String, Item>();
+//        this.al = new ArrayList<Item>();
+//    }
+//
+//    public void addItem(String name, int cost) {
+//        Item it = new Item(name, cost);
+//        hs.put(name, it);
+//        al.add(it);
+//    }
+//
+//    public int voucherWaste(int value) {
+//        SmallestWaste = value;
+//        int[] priceList = new int[al.size()];
+//        for (int i = 0; i < al.size(); i++) {
+//            priceList[i] = al.get(i).cost;
+//        }
+//        buyToEnd(priceList, 0, value);
+//        System.out.println(SmallestWaste);
+//        return SmallestWaste;
+//
+//        /*
+//         * Every product have two filed:
+//         *   1. how many can the voucher buy?
+//         *   2. how much money left?
+//         * */
+//    }
+//
+//    public void buyToEnd(int prices[], int i, int leftMoney) {
+//        if (this.SmallestWaste == 0) {
+//            return;
+//        }
+//        System.out.println("i:" + i + " leftMoney:" + leftMoney);
+//        if (i == prices.length - 1) {
+//            if (this.SmallestWaste > leftMoney % prices[i]) {
+//                this.SmallestWaste = leftMoney % prices[i];
+//            }
+//            return;
+//        }
+//        for (int j = leftMoney / prices[i]; j >= 0; j--) {
+//            int nextLeft = leftMoney - prices[i] * j;
+//            buyToEnd(prices, i + 1, nextLeft);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Cost cost = new Cost();
+//        cost.addItem("A", 243);
+//        cost.addItem("B", 401);
+//        cost.addItem("C", 245);
+//        cost.addItem("D", 1004);
+//        cost.addItem("E", 297);
+//        cost.voucherWaste(2007);
+//    }
+//
+//    public void printCost() {
+//        al.sort(Item::compareTo);
+//        for (Item item : al) {
+//            System.out.println(item.toString());
+//        }
+//    }
+//
+//
+//
+//
+//    public class Item implements Comparable<Item> {
+//        String name;
+//        int cost;   // in cents
+//
+//        public Item(String n, int c) {
+//            name = n;
+//            cost = c;
+//        }
+//
+//        public String toString() {
+//            return name + " " + cost;
+//        }
+//
+//        public int compareTo(Item item) {
+//            return this.cost - item.cost;
+//        }
+//    }
+//}
 //code from Harvey
 
 //package comp1110.homework.J14;
